f68f5f647782b3282af17540054f10a44a48c750
Dasha Mezentseva <dashamezen9988@gmail.com>
2020-04-03 11:47:59 +0300

    Add docker

---
+++ dockerRun.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+filepath=$PWD
+cd $filepath
+mkdir deploy
+cd deploy
+# Download the war from gihub
+curl -LOk https://github.com/DashaMezentseva/Rest/raw/master/target/SpringInAction-1.0-SNAPSHOT.war
+# run the docker
+winpty docker run -it -p 80:8080 -v /$(pwd):/usr/local/tomcat/webapps Rest/java-restfull-api:1.0
+# you can reach this service through docker machine ip, also add the demorest folder to the path
+# to find docker-machine ip execute the following command
+# docker-machine ls
+# example URL http://192.168.99.100/demorest/
